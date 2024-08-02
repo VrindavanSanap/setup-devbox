17f82f9f412996ed35cec3932932bf5e7b5c890e
Vrindavan Sanap <66788089+VrindavanSanap@users.noreply.github.com>
2024-08-02 22:33:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ cd ~
 git clone https://github.com/VrindavanSanap/configuration.git
 rm -rf ./configuration/.git
 mv ./configuration/* .
+for file in (N) *(.); do source "$file"; done
+rm -rf ./configuration
